02d772b2a3c0b73c3ccd7e1a3eb54adfe2c3ad38
史博闻 <shibowen@shibowendeMBP.fios-router.home>
2019-01-29 16:11:28 -0500

    convert java to kt file

--- app/src/main/java/com/gwu/android/gitexercise/HelloMyAndroid.java
+++
@@ -1,7 +0,0 @@
-package com.gwu.android.gitexercise;
-
-public class HelloMyAndroid {
-    public void HelloWorld(){
-        System.out.println("hello my andriod");
-    }
-}
---
+++ app/src/main/java/com/gwu/android/gitexercise/HelloMyAndroid.kt
@@ -0,0 +1,7 @@
+package com.gwu.android.gitexercise
+
+class HelloMyAndroid {
+    fun HelloWorld() {
+        println("hello my andriod")
+    }
+}
