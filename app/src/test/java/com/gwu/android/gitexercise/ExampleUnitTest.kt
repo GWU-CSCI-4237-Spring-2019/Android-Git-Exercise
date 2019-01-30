6a3e4eb08aa613002fbdea06bcab783f55aac717
Larissa <42724886+larissa10001@users.noreply.github.com>
2019-01-29 21:09:27 -0500

    Update ExampleUnitTest.kt
    Java to Kotlin

--- app/src/test/java/com/gwu/android/gitexercise/ExampleUnitTest.kt
+++ app/src/test/java/com/gwu/android/gitexercise/ExampleUnitTest.kt
@@ -1,17 +1,36 @@
-package com.gwu.android.gitexercise
+package com.example.helloworld
 
-import org.junit.Test
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 
-import org.junit.Assert.*
+class MainActivity : AppCompatActivity() {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // you do not need to specify the variable type
+        //vals are like finals in java (can't reassign)
+        val string: String = "hello world"
+        // string = "new stuff" // does not work
+        // vars can be assigned
+        var string2: String? = "creating variables"
+        string2 = "new stuff"
+        string2 = null
+
+        if(string2 != null){
+            val length = string2.length
+        }
+
+        private fun getStringLength(string: String?): Int{
+           // . calls the method only if the string is not null otherwise
+            return string?.length  ?:0 //this is to return null if the string is not null, otherwise return 0
+
+            // same as above code
+            if(string !=null){
+                return string.length
+            }
+            return 0
+        }
     }
 }
